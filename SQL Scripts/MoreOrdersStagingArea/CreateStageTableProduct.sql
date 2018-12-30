b49ea707f027c587b9d7a6949d9dd2b555fa9e2e
DESKTOP-R3VH6HI\piotr <piotrkaweckix@gmail.com>
2018-12-30 23:13:09 +0000

    created staging area product

--- SQL Scripts/MoreOrdersStagingArea/CreateStageTableProduct.sql
+++ SQL Scripts/MoreOrdersStagingArea/CreateStageTableProduct.sql
@@ -2,12 +2,12 @@ IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'Product')
 	CREATE TABLE [dbo].[Product]
 	(
 	[ProductID] [char](11),
-	[Description] [char](11),
-	[Color] [char](11),
-	[AvaialblePortal] [char](50),
-	[BoxID] [char](11),
-	[Active] [char](1),
-	[Exclusive] [char](1),
+	[Description] [nvarchar](255),
+	[Color] [nvarchar](255),
+	[AvaialblePortal] [bit],
+	[BoxID] [int],
+	[Active] [bit],
+	[Exclusive] [bit],
 	[CreateDate] [datetime],
 	[LastUpdateDate] [datetime]
 	)
