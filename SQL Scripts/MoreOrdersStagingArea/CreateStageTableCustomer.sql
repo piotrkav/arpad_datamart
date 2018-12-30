c6e390310b9c99bd079014fdbabbbb927a5de67b
DESKTOP-R3VH6HI\piotr <piotrkaweckix@gmail.com>
2018-12-30 20:03:58 +0000

    added customer to staging area

--- SQL Scripts/MoreOrdersStagingArea/CreateStageTableCustomer.sql
+++ SQL Scripts/MoreOrdersStagingArea/CreateStageTableCustomer.sql
@@ -5,11 +5,13 @@ IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'Customer')
 		[CustomerTypeDescription] [nvarchar](50),
 		[Discount] [tinyint],
 		[DateOfBirthDate] [datetime],
-		[Gender] [char](1),
+		[Gender] [nchar](1),
 		[GeographicRegion] [nvarchar](50),
 		[Language] [nvarchar](50),
 		[VipCustomer] [char](1),
-		[Country] [nvarchar](50)
+		[Country] [nvarchar](50),
+		[CountryCode] [nvarchar](2),	
+		[EuroZone] [bit]
 	)
 ELSE
 	TRUNCATE TABLE Customer
