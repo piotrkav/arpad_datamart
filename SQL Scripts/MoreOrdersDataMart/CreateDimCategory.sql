02e65b246dd84d157449aaddd1203b78ca509713
DESKTOP-R3VH6HI\piotr <piotrkaweckix@gmail.com>
2019-01-02 16:24:30 +0000

    DimPaymentMethod, DimSize, DimCategory, DimAddress added

--- SQL Scripts/MoreOrdersDataMart/CreateDimCategory.sql
+++ SQL Scripts/MoreOrdersDataMart/CreateDimCategory.sql
@@ -1,8 +1,8 @@
 IF NOT EXISTS (SELECT name FROM sys.tables where name = 'DimCategory')
 	CREATE TABLE [dbo].[DimCategory](
 	[CategoryKey] [int] IDENTITY(1,1) NOT NULL,
-	[CategoryID] [char](11) NOT NULL,
-	[ParentCategoryID] [char](11) NOT NULL,
+	[CategoryID] [int] NOT NULL,
+	--[ParentCategoryID] [int NOT NULL,
 	[Name] [nvarchar](50) NULL,
-	[Gender] [char](1) NULL
+	[Gender] [nchar](1) NULL
 		) ON [PRIMARY]
